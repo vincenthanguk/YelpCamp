086f7b9fe894ffea9949815800ee8d9f344ba3f0
vincenthanguk <vincentkreusel@gmail.com>
2021-11-06 11:07:03 +0100

    added styled home page

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
-  res.redirect('/campgrounds');
+  res.render('home');
 });
 
 // only runs if no other route is matched first
